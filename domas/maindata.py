a6783a9a050219d49ee840c4c4b3d74aabe56322
Domas <syaifoel.domas@gmail.com>
2018-03-15 12:31:33 +0100

    Wow

--- domas/maindata.py
+++ domas/maindata.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 1000
+n = 50
 
 a = np.genfromtxt('datamatlab.txt')
 t = a.T
-
-n = n / 30
 
 t1 = t[0:860]
 t2 = t[1780:2040]
@@ -15,12 +13,12 @@ t4 = t[3800:4040]
 t5 = t[5280:5380]
 t6 = t[7750:8300]
 
-t1 = t1[0::int(170/n)]
-t2 = t2[0::int(152/n)]
-t3 = t3[0::int(128/n)]
-t4 = t4[0::int(148/n)]
-t5 = t5[0::int(120/n)]
-t6 = t6[0::int(1110/n)]
+t1 = t1[0::int(170/n)+1]
+t2 = t2[0::int(152/n)+1]
+t3 = t3[0::int(128/n)+1]
+t4 = t4[0::int(148/n)+1]
+t5 = t5[0::int(120/n)+1]
+t6 = t6[0::int(1110/n)+1]
 
 y = np.concatenate((t1, t2, t3, t4, t5, t6))
 print(len(y))
